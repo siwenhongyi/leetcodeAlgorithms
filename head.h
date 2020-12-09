b130a2eb780ff623a7bf11df8710d38147ce18df
siwenhongyi <zmlhongyi@vip.qq.com>
2020-12-09 21:37:38 +0800

    first ci

---
+++ head.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <set>
+#include <map>
+#include <stack>
+#include <bitset>
+#include <string>
+#include <strstream>
+#include <queue>
+#include <vector>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <random>
+#include <sstream>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+// my
+
+#include "LeetCode_func.h"
