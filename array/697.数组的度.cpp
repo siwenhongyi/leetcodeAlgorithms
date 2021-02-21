99d4a36a91436d043aade61b317ea65a2d14dca1
siwenhongyi <zmlhongyi@vip.qq.com>
2021-02-21 16:13:16 +0800

    2021.02.20

---
+++ array/697.数组的度.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=697 lang=cpp
+ *
+ * [697] 数组的度
+ */
+#include "head.h"
+// @lc code=start
+class Solution {
+ public:
+  int findShortestSubArray(vector<int>& nums) {
+    map<int, vector<int>> pos;
+    map<int, int> fre;
+    int est = 0, n = nums.size();
+    for (int i = 0; i < n; i++) {
+      est = max(est, ++fre[nums[i]]);
+      pos[nums[i]].push_back(i);
+    }
+    int res = INT_MAX;
+    for (auto& it : pos) {
+      auto&& [a, b] = it;
+      if (b.size() < est) continue;
+      res = min(res, b[est - 1] - b[0] + 1);
+    }
+    return res;
+  }
+};
+// @lc code=end
---
+++ dynamic-programming/63.不同路径-ii.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=cpp
+ *
+ * [63] 不同路径 II
+ */
+#include "head.h"
+// @lc code=start
+class Solution {
+ public:
+  int uniquePathsWithObstacles(vector<vector<int>>& obs) {
+    if (obs[0][0] || obs.back().back()) return 0;
+    int n = obs.size(), m = obs[0].size(), t;
+    vector<vector<int>> res(n, vector<int>(m, 0));
+    for (int i = 0; i < m && !obs[0][i]; i++) res[0][i] = 1;
+    for (int i = 0; i < n && !obs[i][0]; i++) res[i][0] = 1;
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < m; j++) {
+        res[i][j] = obs[i][j] ? 0 : res[i - 1][j] + res[i][j - 1];
+      }
+    }
+    return res.back().back();
+  }
+};
+// @lc code=end
