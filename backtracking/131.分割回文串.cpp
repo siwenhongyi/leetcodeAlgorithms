25ee56d2d61a1dccf6d4e4e59d3f2a53818b21c5
siwenhongyi <zmlhongyi@vip.qq.com>
2021-02-21 21:30:14 +0800

    2021.02.21

---
+++ backtracking/131.分割回文串.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=131 lang=cpp
+ *
+ * [131] 分割回文串
+ */
+#include "head.h"
+// @lc code=start
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+
+    }
+};
+// @lc code=end
+
