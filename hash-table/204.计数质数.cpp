170780a151137149772c4852dc355f54ba0e69ec
siwenhongyi <zmlhongyi@vip.qq.com>
2021-02-25 23:30:03 +0800

    ci

--- hash-table/204.计数质数.cpp
+++ hash-table/204.计数质数.cpp
@@ -1,38 +1,35 @@
 /*
-* @lc app=leetcode.cn id=204 lang=cpp
-*
-* [204] 计数质数
-*/
+ * @lc app=leetcode.cn id=204 lang=cpp
+ *
+ * [204] 计数质数
+ */
 
-#include"head.h"
+#include "head.h"
 using namespace std;
 // @lc code=start
 class Solution {
-public:
-    int countPrimes(int n) {
-        if (n <= 2)return 0;
-        vector<unsigned int> p(n / 32 + 1, 0xffffffff);
-        int res = 0;
-        int c, r;
-        //string x;
-        for (int i = 2; i < n; i++) {
-            c = i / 32, r = i % 32;
-            //x = bitset<32>(p[c]).to_string();
-            if ((p[c] & (1 << r)) == 0)
-                continue;
-            res++;
-            //cout << i << ' ';
-            long long int j = (long long)i * i;
-            while (j < n) {
-                c = j / 32, r = j % 32;
-                p[c] &= (0xffffffff ^ (1 << r));
-                //x = bitset<32>(p[c]).to_string();
-                j += i;
-            }
-        }
-        return res;
-
+ public:
+  int countPrimes(int n) {
+    if (n <= 2) return 0;
+    vector<unsigned int> p(n / 32 + 1, 0xffffffff);
+    int res = 0;
+    int c, r;
+    // string x;
+    for (int i = 2; i < n; i++) {
+      c = i / 32, r = i % 32;
+      // x = bitset<32>(p[c]).to_string();
+      if ((p[c] & (1 << r)) == 0) continue;
+      res++;
+      // cout << i << ' ';
+      long long int j = (long long)i * i;
+      while (j < n) {
+        c = j / 32, r = j % 32;
+        p[c] &= (0xffffffff ^ (1 << r));
+        // x = bitset<32>(p[c]).to_string();
+        j += i;
+      }
     }
+    return res;
+  }
 };
 // @lc code=end
-
