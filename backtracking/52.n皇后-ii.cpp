0f4e0a62afb97345a4ddaeb9dc98f73b5338a43d
siwenhongyi <zmlhongyi@qq.com>
2022-03-14 01:13:43 +0800

    N queue two

---
+++ backtracking/52.n皇后-ii.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=52 lang=cpp
+ *
+ * [52] N皇后 II
+ */
+#include "head.h"
+using namesapce std;
+// @lc code=start
+class Solution {
+public:
+    int totalNQueens(int n) {
+
+    }
+};
+// @lc code=end
+
