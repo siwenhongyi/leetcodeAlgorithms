81f1f57abcad8abfc6c4b7fae5486c5a7ee30cd3
siwenhongyi <zmlhongyi@qq.com>
2022-03-14 00:47:57 +0800

    N queue

--- backtracking/51.n-皇后.cpp
+++ backtracking/51.n-皇后.cpp
@@ -7,31 +7,70 @@
 using namesapce std;
 // @lc code=start
 class Solution {
-    private:
-    const int N = 17;
-    const int M = 8;
-    vector<int> row,col,left,right;
+private:
+    int row, col, left, right, n;
     vector<vector<string>> res;
-    int  get_status(int x,int y) {
-        return ~((row[x])|(col[y])|(left[x-y+M])|(right[y-x+M]);)
+   unordered_set<int> used;
+
+    bool  get_status(int x, int y) {
+        return ((row & (1 << x)) || (col & (1 << y)) || (left & (1 << (y + x + 8))) || (right & (1 << (x - y + 8))));
     }
-    void dfs(vector<string>& path,x,y,int cnt) {
-        if(!cnt) return;
-        auto&& status = get_status(x,y);
-        for(int i = 0;i<N;i++) {
-            if (!(status ^ (1 << i)))
-            continue;
-            path[]
+    void set_bit(const int& x, const  int& y, bool is_fill) {
+        if (is_fill) {
+            row |= (1 << x);
+            col |= (1 << y);
+            left |= (1 << (y + x + 8));
+            right |= (1 << (x - y + 8));
+        }
+        else {
+            row ^= (1 << x);
+            col ^= (1 << y);
+            left ^= (1 << (y + x + 8));
+            right ^= (1 << (x - y + 8));
+
         }
     }
+    void dfs(vector<string>& path, const int& x, const int& y, int cnt) {
+        path[x][y] = 'Q';
+        set_bit(x, y, true);
+        cnt--;
+        if (!cnt) {
+            int uuid = 0, mod = 1;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (path[i][j] == 'Q') {
+                        uuid += (j * mod);
+                        mod *= 10;
+                        break;
+                    }
+            if (!used.count(uuid)) {
+                res.push_back(path);
+                used.insert(uuid);
+            }
+            path[x][y] = '.';
+            set_bit(x, y, false);
+            return;
+        }
+        for (int i = x+1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (get_status(i, j)) continue;
+                dfs(path, i, j, cnt);
+            }
+        }
+        path[x][y] = '.';
+        set_bit(x, y, false);
+    }
 public:
-Solution():row(vector<int>(N,0)),
-col(vector<int>(N,0)),
-right(vector<int>(N,0)),
-left(vector<int>(N,0)){}
+    Solution() :row(0), col(0), right(0), left(0) {}
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> path(n,string(n,'.'));
-        dfs(path,0,0,n);
+        this->n = n;
+        vector<string> path(n, string(n, '.'));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (used.count(i * n + j)) continue;
+                dfs(path, i, j, n);
+            }
+        }
         return res;
     }
 };
