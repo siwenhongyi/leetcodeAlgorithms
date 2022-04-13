de774ca3729616f6a3869ecae1599379f2727ec6
siwenhongyi <zmlhongyi@qq.com>
2022-04-13 17:12:29 +0800

    ci

--- backtracking/89.格雷编码.cpp
+++ backtracking/89.格雷编码.cpp
@@ -9,6 +9,12 @@ using namespace std;
 // @lc code=start
 class Solution {
  public:
-  vector<int> grayCode(int n) { return {}; }
+  vector<int> grayCode(int n) {
+    vector<int> ret(1 << n);
+    for (int i = 0; i < ret.size(); i++) {
+      ret[i] = (i >> 1) ^ i;
+    }
+    return ret;
+  }
 };
 // @lc code=end
